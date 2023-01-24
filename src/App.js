a2a02b9a9a2108be67e6b336819392c89626afd8
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2023-01-24 07:48:24 +0100

    w

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Brands from './components/Brands';
+import Me from './components/Me';
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
     <Header/>
     <Hero/>
     <Brands/>
+    <Me/>
     <div style={{height: '2000px'}}></div>
     </div>
     );
